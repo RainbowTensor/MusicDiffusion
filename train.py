05cc2b7e55bd85fd79b49f4153a710e42f1fc55d
Nino Hasl <nino.hasl2@gmail.com>
2023-07-18 14:21:08 +0200

    1d autoencoder

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import numpy as np
 import os
 
 from dataset import LakhPrmat2cLMDB
-from movq.autoencoder import Autoencoder
+from autoencoder.autoencoder import Autoencoder
 
 
 with open("./config.yaml", "r") as f:
